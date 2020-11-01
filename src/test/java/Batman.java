9dee727b4832583dacab98918c048e1dc43b4380
IssB20 <islem_ouiddir@yahoo.fr>
2020-11-01 11:24:17 -0500

    Batman has Bat Bike

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -5,6 +5,7 @@ public class Batman {
         System.out.println("I will catch you Joker");
         // Yes he will catch him
         // Batman has cool bat mobile
+        // batman has bat Bike
 
     }
 }
